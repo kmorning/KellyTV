525335e0116bf2b27ab930eaee7a18b67a644287
kmorning <kleemorning@gmail.com>
2018-02-24 18:09:07 -0500

    Moved guided step fragment to it's own class

---
+++ app/src/main/java/org/duckdns/altered/kellytv/EpgSettingsFragment.java
@@ -0,0 +1,131 @@
+package org.duckdns.altered.kellytv;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v17.leanback.app.GuidedStepFragment;
+import android.support.v17.leanback.widget.GuidanceStylist;
+import android.support.v17.leanback.widget.GuidedAction;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by kmorning on 2018-02-20.
+ */
+
+public class EpgSettingsFragment extends GuidedStepFragment {
+    /* Action ID definition */
+    private static final int ACTION_URL = 0;
+    private static final int ACTION_AUTO_UPDATE = 1;
+    private static final int ACTION_UPDATE_INT_UNITS = 2;
+    private static final int ACTION_UPDATE_INT_VALUE = 3;
+
+    /* Auto update on/off options */
+    private static final int UPDATE_OPTION_CHECK_SET_ID = 10;
+    private static final String[] UPDATE_OPTION_NAMES = {"on", "off"};
+    //private static final boolean[] UPDATE_OPTION_CHECKED = {true, false};
+    //private boolean[] checked = new boolean[2];
+
+    private boolean bAutoUpdate;
+
+    @Override
+    public GuidanceStylist.Guidance onCreateGuidance(Bundle savedInstanceState) {
+        String title = "EPG Settings";
+        String breadcrumb = "";
+        String description = "";
+        Drawable icon = getActivity().getDrawable(R.drawable.ic_main_icon);
+
+        return new GuidanceStylist.Guidance(title, description, breadcrumb, icon);
+    }
+
+    @Override
+    public void onCreateActions(@NonNull List<GuidedAction> actions, Bundle savedInstanceState) {
+        Context context = getActivity();
+
+        addActionWithSub(actions, context, ACTION_AUTO_UPDATE, "Auto Update", "");
+        addEditableAction(actions, context, ACTION_URL, "EPG URL", "http://");
+        addEditableAction(actions, context, ACTION_UPDATE_INT_VALUE, "Update Interval", "5");
+
+        loadSettings();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        GuidedAction autoUpdateAction = findActionById(ACTION_AUTO_UPDATE);
+        List<GuidedAction> subActions = autoUpdateAction.getSubActions();
+        boolean checked[] = new boolean[2];
+        checked[0] = bAutoUpdate;
+        checked[1] = !bAutoUpdate;
+        for (int i = 0; i < UPDATE_OPTION_NAMES.length; i++) {
+            addCheckedAction(subActions, getActivity(), UPDATE_OPTION_NAMES[i], checked[i]);
+        }
+        if (bAutoUpdate) {
+            autoUpdateAction.setDescription(UPDATE_OPTION_NAMES[0]);
+        } else {
+            autoUpdateAction.setDescription(UPDATE_OPTION_NAMES[1]);
+        }
+    }
+
+    @Override
+    public void onGuidedActionClicked(GuidedAction action) {
+        if (ACTION_URL == action.getId()) {
+            Log.d("editedText", action.getDescription().toString());
+        } else if (ACTION_UPDATE_INT_VALUE == getId()) {
+            Log.d("editedText", action.getDescription().toString());
+        }
+    }
+
+    @Override
+    public boolean onSubGuidedActionClicked(GuidedAction action) {
+        if (action.isChecked()) {
+            String selection = action.getTitle().toString();
+            findActionById(ACTION_AUTO_UPDATE).setDescription(selection);
+            notifyActionChanged(findActionPositionById(ACTION_AUTO_UPDATE));
+            int i = Arrays.asList(UPDATE_OPTION_NAMES).indexOf(selection);
+            bAutoUpdate = i == 0;
+        }
+        return true;
+    }
+
+    private void loadSettings() {
+        // TODO: Load saved settings here
+        // For now just set to auto update on for now and url to something
+        bAutoUpdate = true;
+    }
+
+    private static void addActionWithSub(List<GuidedAction> actions, Context context, long id,
+                                         String title, String desc) {
+        List<GuidedAction> subActions = new ArrayList();
+        actions.add(new GuidedAction.Builder(context)
+                .title(title)
+                .id(id)
+                .description(desc)
+                .subActions(subActions)
+                .build());
+    }
+
+    private static void addEditableAction(List<GuidedAction> actions, Context context, long id,
+                                          String title, String desc) {
+        actions.add(new GuidedAction.Builder(context)
+                .id(id)
+                .title(title)
+                .description(desc).descriptionEditable(true)
+                .build());
+    }
+
+    private static void addCheckedAction(List<GuidedAction> actions, Context context,
+                                         String title, boolean checked) {
+        GuidedAction guidedAction = new GuidedAction.Builder(context)
+                .title(title)
+                .checkSetId(UPDATE_OPTION_CHECK_SET_ID)
+                .build();
+        guidedAction.setChecked(checked);
+        actions.add(guidedAction);
+
+    }
+}
